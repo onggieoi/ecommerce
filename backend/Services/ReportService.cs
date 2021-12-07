f92a5f1cd9a7c9b70d35c85eaae39e7d5f42444f
quangduccao <onggieoi@gmail.com>
2021-12-07 19:21:00 +0700

    add report image and formated

--- backend/Services/ReportService.cs
+++ backend/Services/ReportService.cs
@@ -14,10 +14,12 @@ namespace backend.Services;
 public class ReportService : IReportService
 {
   private ApplicationDbContext _dbContext;
+  private IAuthenticatedUserService _userContext;
 
-  public ReportService(ApplicationDbContext dbContext)
+  public ReportService(ApplicationDbContext dbContext, IAuthenticatedUserService userContext)
   {
     _dbContext = dbContext;
+    _userContext = userContext;
   }
 
   public async Task<DashboardReport> GetReportDashboradAsync()
@@ -97,11 +99,11 @@ public class ReportService : IReportService
     {
       IWorkbook workbook = new XSSFWorkbook();
 
-      ISheet sheet1 = workbook.CreateSheet("Sheet1");
+      ISheet sheet1 = workbook.CreateSheet("SNKR Export");
 
       sheet1.AddMergedRegion(new CellRangeAddress(0, 0, 0, 10));
       IRow titleRow = sheet1.CreateRow(0);
-      titleRow.Height = 30 * 80;
+      titleRow.Height = 10 * 60;
 
       var titleCell = titleRow.CreateCell(0);
       var titleStyle = workbook.CreateCellStyle();
@@ -109,23 +111,50 @@ public class ReportService : IReportService
       IFont titlFont = workbook.CreateFont();
       titlFont.IsBold = true;
       titlFont.FontHeightInPoints = 20;
-      titleStyle.SetFont(titlFont);
 
-      titleCell.SetCellValue($"Order Report SKNR {query.CreatedFrom.ToString("yyyyMMdd")} - {query.CreatedTo.ToString("yyyyMMdd")}");
+      titleStyle.SetFont(titlFont);
+      titleStyle.VerticalAlignment = VerticalAlignment.Center;
+      titleStyle.Alignment = HorizontalAlignment.Center;
+
+      titleCell.SetCellValue($"Order Report SKNR");
       titleCell.CellStyle = titleStyle;
 
-      var rowIndex = 1;
+      IRow authorRow = sheet1.CreateRow(2);
+
+      authorRow.CreateCell(1).SetCellValue("Created by");
+      authorRow.CreateCell(3).SetCellValue(_userContext.UserName);
+
+      IRow createAtRow = sheet1.CreateRow(3);
+
+      createAtRow.CreateCell(1).SetCellValue("Create At (UTC)");
+      createAtRow.CreateCell(3).SetCellValue(DateTime.UtcNow.ToString("dd/MM/yyyy"));
+
+      IRow createdFromRow = sheet1.CreateRow(5);
+
+      createdFromRow.CreateCell(1).SetCellValue("Created From");
+      createdFromRow.CreateCell(3).SetCellValue(query.CreatedFrom.ToString("dd/MM/yyyy"));
+
+      IRow createdToRow = sheet1.CreateRow(6);
+
+      createdToRow.CreateCell(1).SetCellValue("Created To");
+      createdToRow.CreateCell(3).SetCellValue(query.CreatedTo.ToString("dd/MM/yyyy"));
+
+      var rowIndex = 8;
 
       IRow row = sheet1.CreateRow(rowIndex);
-      row.CreateCell(0).SetCellValue("STT");
-      row.CreateCell(1).SetCellValue("Total Price");
-      row.CreateCell(2).SetCellValue("Coupon Code");
-      row.CreateCell(3).SetCellValue("Discount");
-      row.CreateCell(4).SetCellValue("User");
-      row.CreateCell(5).SetCellValue("Address");
-      row.CreateCell(6).SetCellValue("Card");
-      row.CreateCell(7).SetCellValue("Contact");
-      row.CreateCell(8).SetCellValue("Create At");
+
+      var headerStyle = workbook.CreateCellStyle();
+      headerStyle.FillBackgroundColor = IndexedColors.Grey25Percent.Index;
+      headerStyle.FillPattern = FillPattern.SolidForeground;
+
+      List<string> headers = new List<string> { "STT", "Total Price", "Coupon Code", "Discount", "User", "Address", "Card", "Contact", "Create At", };
+
+      for (int i = 0; i < headers.Count(); i++)
+      {
+        var headerCell = row.CreateCell(i);
+        headerCell.SetCellValue(headers[i]);
+        headerCell.CellStyle = headerStyle;
+      }
 
       for (int i = 0; i < reports.Count(); i++)
       {
@@ -134,16 +163,31 @@ public class ReportService : IReportService
 
         newVal.CreateCell(0).SetCellValue(i + 1);
         newVal.CreateCell(1).SetCellValue(reports[i].TotalPrice);
-        newVal.CreateCell(2).SetCellValue(reports[i].Coupon is null ? reports[i].Coupon.Code : "");
-        newVal.CreateCell(3).SetCellValue(reports[i].Coupon is null ? reports[i].Coupon.Discount.ToString() : "");
+        newVal.CreateCell(2).SetCellValue(reports[i].Coupon is not null ? reports[i].Coupon.Code : "");
+        newVal.CreateCell(3).SetCellValue(reports[i].Coupon is not null ? reports[i].Coupon.Discount.ToString() : "");
         newVal.CreateCell(4).SetCellValue(reports[i]?.User.UserName);
         newVal.CreateCell(5).SetCellValue(reports[i]?.Address.Info);
-        newVal.CreateCell(6).SetCellValue(reports[i]?.Card.CardType);
+        newVal.CreateCell(6).SetCellValue(reports[i]?.Card is null ? "Cash" : reports[i]?.Card.Name + " - " + reports[i]?.Card.CardType + ":" + "**** **** ****" + reports[i]?.Card.LastFourDigit);
         newVal.CreateCell(7).SetCellValue(reports[i]?.Contact.Number);
-        newVal.CreateCell(8).SetCellValue(reports[i].CreateAt.ToString("yyyyMMdd"));
+        newVal.CreateCell(8).SetCellValue(reports[i].CreateAt.ToString("dd/MM/yyyy"));
 
         sheet1.AutoSizeColumn(i + 1);
+        GC.Collect();
       }
+
+      sheet1.SetColumnWidth(5, 9000);
+      sheet1.SetColumnWidth(6, 7000);
+      sheet1.SetColumnWidth(7, 4000);
+
+      byte[] data = File.ReadAllBytes("images/snkr.png");
+      int pictureIndex = workbook.AddPicture(data, PictureType.PNG);
+      ICreationHelper helper = workbook.GetCreationHelper();
+      IDrawing drawing = sheet1.CreateDrawingPatriarch();
+      IClientAnchor anchor = helper.CreateClientAnchor();
+      anchor.Col1 = 6;
+      anchor.Row1 = 2;
+      IPicture picture = drawing.CreatePicture(anchor, pictureIndex);
+      picture.Resize();
 
       workbook.Write(fs);
 
